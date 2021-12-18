0cf69eeb231ce651c58c4ec70898dd0b196ee501
aprilahijriyan <37798612+aprilahijriyan@users.noreply.github.com>
2021-12-19 05:32:13 +0700

    👌 IMPROVE: Nice endpoint name, add default tags & add default summary

--- fastack/controller.py
+++ fastack/controller.py
@@ -15,14 +15,25 @@ class Controller:
     mapping_endpoints = MAPPING_ENDPOINTS
     method_endpoints = METHOD_ENDPOINTS
 
+    def get_endpoint_name(self) -> str:
+        c_name = "Controller"
+        name = type(self).__name__
+        if name.endswith(c_name):
+            name = name.rstrip(c_name)
+
+        rv = ""
+        for c in name:
+            if c.isupper() and len(rv) > 0:
+                c = "-" + c
+            rv += c
+
+        return rv.lower()
+
     def get_url_prefix(self) -> str:
-        c_name = "controller"
         prefix = self.url_prefix
         if not prefix:
-            prefix = type(self).__name__.lower()
-            if prefix.endswith(c_name):
-                prefix = prefix.rstrip(c_name)
-        return prefix
+            prefix = self.get_endpoint_name()
+        return "/" + prefix.lstrip("/")
 
     def get_path(self, method: str) -> str:
         return self.mapping_endpoints.get(method) or ""
@@ -31,6 +42,8 @@ class Controller:
         return self.method_endpoints.get(method) or None
 
     def build(self, **kwds) -> APIRouter:
+        endpoint_name = self.get_endpoint_name().replace("-", " ").title()
+        kwds.setdefault("tags", [endpoint_name])
         kwds.setdefault("prefix", self.get_url_prefix())
         router = APIRouter(**kwds)
         for method_name in dir(self):
@@ -43,9 +56,12 @@ class Controller:
                 http_method = self.get_http_method(method_name)
 
             if http_method:
+                summary = f"{endpoint_name} {method_name.title()}"
                 path = self.get_path(method_name)
                 params = getattr(func, "__route_params__", None) or {}
                 params.setdefault("methods", [http_method])
+                params.setdefault("name", method_name)
+                params.setdefault("summary", summary)
                 router.add_api_route(path, func, **params)
 
         return router
