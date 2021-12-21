d12f9fd2d83577a9e93c1e5b7bf64be1bff274f0
aprilahijriyan <37798612+aprilahijriyan@users.noreply.github.com>
2021-12-22 04:01:54 +0700

    👌 IMPROVE: Use our session object to open database transactions (atomic transactions)

--- examples/sqlmodel/app/controllers/book/__init__.py
+++ examples/sqlmodel/app/controllers/book/__init__.py
@@ -3,12 +3,12 @@ from typing import List
 from app.models import Book
 from fastapi import Request, status
 from fastapi.responses import Response
-from pydantic import BaseModel, constr
-from sqlalchemy.orm.session import SessionTransaction
+from pydantic import BaseModel, conint, constr
 from sqlalchemy.sql.expression import desc
-from sqlmodel import Session, select
+from sqlmodel import select
 
 from fastack import CRUDController
+from fastack.plugins.sqlmodel.session import Session
 
 
 class BodyBookModel(BaseModel):
@@ -18,8 +18,8 @@ class BodyBookModel(BaseModel):
 
 class BookController(CRUDController):
     def retrieve(self, request: Request, id: int) -> Response:
-        with request.state.db.open() as session:
-            session: Session
+        session: Session = request.state.db.open()
+        with session:
             qs = select(Book).where(Book.id == id)
             book: Book = session.exec(qs).first()
             if not book:
@@ -27,27 +27,27 @@ class BookController(CRUDController):
 
             return self.json("Detail book", book)
 
-    def list(self, request: Request, page: int = 1, page_size: int = 10) -> Response:
-        with request.state.db.open() as session:
-            session: Session
+    def list(
+        self, request: Request, page: conint(gt=0) = 1, page_size: conint(gt=0) = 10
+    ) -> Response:
+        session: Session = request.state.db.open()
+        with session:
             books: List[Book] = session.exec(
                 select(Book).order_by(desc(Book.date_created))
             ).all()
             return self.get_paginated_response(books, page, page_size)
 
     def create(self, request: Request, body: BodyBookModel) -> Response:
-        with request.state.db.atomic() as conn:
-            conn: SessionTransaction
-            session: Session = conn.session
+        session: Session = request.state.db.open()
+        with session.begin():
             data = body.dict()
             book: Book = Book.create(session, **data)
 
         return self.json("Created", book)
 
     def update(self, request: Request, id: int, body: BodyBookModel) -> Response:
-        with request.state.db.atomic() as conn:
-            conn: SessionTransaction
-            session: Session = conn.session
+        session: Session = request.state.db.open()
+        with session.begin():
             book = session.exec(select(Book).where(Book.id == id)).first()
             if not book:
                 return self.json("Not found", status=status.HTTP_404_NOT_FOUND)
@@ -58,9 +58,8 @@ class BookController(CRUDController):
         return self.json("Updated", book)
 
     def destroy(self, request: Request, id: int) -> Response:
-        with request.state.db.atomic() as conn:
-            conn: SessionTransaction
-            session: Session = conn.session
+        session: Session = request.state.db.open()
+        with session.begin():
             book: Book = session.exec(select(Book).where(Book.id == id)).first()
             if not book:
                 return self.json("Not found", status=status.HTTP_404_NOT_FOUND)
