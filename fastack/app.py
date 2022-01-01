b8f4cee218dee17f7099714f42937a477a123c53
aprilahijriyan <37798612+aprilahijriyan@users.noreply.github.com>
2022-01-02 02:46:16 +0700

    👌 IMPROVE: Add typing hint for controller.

--- fastack/app.py
+++ fastack/app.py
@@ -1,7 +1,12 @@
 from types import ModuleType
-from typing import Any, Callable, Union
+from typing import Any, Callable, Dict, List, Optional, Sequence, Type, Union
 
-from fastapi import FastAPI
+from fastapi import FastAPI, params, routing
+from fastapi.datastructures import Default
+from fastapi.responses import JSONResponse, Response
+from fastapi.routing import APIRoute
+from starlette.routing import BaseRoute
+from starlette.types import ASGIApp
 from typer import Typer
 
 from .controller import Controller
@@ -33,11 +38,46 @@ class Fastack(FastAPI):
             else:
                 self.cli.command(command.__name__)(command)
 
-    def include_controller(self, controller: Controller, **kwds):
+    def include_controller(
+        self,
+        controller: Controller,
+        *,
+        prefix: str = "",
+        tags: Optional[List[str]] = None,
+        dependencies: Optional[Sequence[params.Depends]] = None,
+        default_response_class: Type[Response] = Default(JSONResponse),
+        responses: Optional[Dict[Union[int, str], Dict[str, Any]]] = None,
+        callbacks: Optional[List[BaseRoute]] = None,
+        routes: Optional[List[routing.BaseRoute]] = None,
+        redirect_slashes: bool = True,
+        default: Optional[ASGIApp] = None,
+        dependency_overrides_provider: Optional[Any] = None,
+        route_class: Type[APIRoute] = APIRoute,
+        on_startup: Optional[Sequence[Callable[[], Any]]] = None,
+        on_shutdown: Optional[Sequence[Callable[[], Any]]] = None,
+        deprecated: Optional[bool] = None,
+        include_in_schema: bool = True,
+    ):
         assert isinstance(
             controller, Controller
         ), f"Controller must be an instance of {controller!r}"
-        router = controller.build(**kwds)
+        router = controller.build(
+            prefix=prefix,
+            tags=tags,
+            dependencies=dependencies,
+            default_response_class=default_response_class,
+            responses=responses,
+            callbacks=callbacks,
+            routes=routes,
+            redirect_slashes=redirect_slashes,
+            default=default,
+            dependency_overrides_provider=dependency_overrides_provider,
+            route_class=route_class,
+            on_startup=on_startup,
+            on_shutdown=on_shutdown,
+            deprecated=deprecated,
+            include_in_schema=include_in_schema,
+        )
         self.include_router(router)
 
 
