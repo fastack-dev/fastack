500aedb3027a63bc7af2fd834532d0430e5f36d0
aprilahijriyan <37798612+aprilahijriyan@users.noreply.github.com>
2022-01-03 07:36:38 +0700

    🎨 Improved formatting: Add FastAPI parameters

--- fastack/app.py
+++ fastack/app.py
@@ -1,10 +1,11 @@
 from types import ModuleType
-from typing import Any, Callable, Dict, List, Optional, Sequence, Type, Union
+from typing import Any, Callable, Coroutine, Dict, List, Optional, Sequence, Type, Union
 
-from fastapi import FastAPI, params, routing
+from fastapi import Depends, FastAPI, Request, params, routing
 from fastapi.datastructures import Default
 from fastapi.responses import JSONResponse, Response
 from fastapi.routing import APIRoute
+from starlette.middleware import Middleware
 from starlette.routing import BaseRoute
 from starlette.types import ASGIApp
 from typer import Typer
@@ -81,18 +82,79 @@ class Fastack(FastAPI):
         self.include_router(router)
 
 
-def create_app(settings: ModuleType, **kwds):
-    kwds.setdefault("title", "Fastack")
-    kwds.setdefault("description", "Fastack Framework")
-    kwds.setdefault(
-        "contact",
-        {
+def create_app(
+    settings: ModuleType,
+    routes: Optional[List[BaseRoute]] = None,
+    title: str = "Fastack",
+    description: str = "Fastack Framework",
+    version: str = "0.1.0",
+    openapi_url: Optional[str] = "/openapi.json",
+    openapi_tags: Optional[List[Dict[str, Any]]] = None,
+    servers: Optional[List[Dict[str, Union[str, Any]]]] = None,
+    dependencies: Optional[Sequence[Depends]] = None,
+    default_response_class: Type[Response] = Default(JSONResponse),
+    docs_url: Optional[str] = "/docs",
+    redoc_url: Optional[str] = "/redoc",
+    swagger_ui_oauth2_redirect_url: Optional[str] = "/docs/oauth2-redirect",
+    swagger_ui_init_oauth: Optional[Dict[str, Any]] = None,
+    middleware: Optional[Sequence[Middleware]] = None,
+    exception_handlers: Optional[
+        Dict[
+            Union[int, Type[Exception]],
+            Callable[[Request, Any], Coroutine[Any, Any, Response]],
+        ]
+    ] = None,
+    on_startup: Optional[Sequence[Callable[[], Any]]] = None,
+    on_shutdown: Optional[Sequence[Callable[[], Any]]] = None,
+    terms_of_service: Optional[str] = None,
+    contact: Optional[Dict[str, Union[str, Any]]] = None,
+    license_info: Optional[Dict[str, Union[str, Any]]] = None,
+    openapi_prefix: str = "",
+    root_path: str = "",
+    root_path_in_servers: bool = True,
+    responses: Optional[Dict[Union[int, str], Dict[str, Any]]] = None,
+    callbacks: Optional[List[BaseRoute]] = None,
+    deprecated: Optional[bool] = None,
+    include_in_schema: bool = True,
+    **extra: Any,
+):
+    if not contact:
+        contact = {
             "name": "Fastack",
             "url": "https://github.com/fastack-dev/fastack",
-        },
+        }
+
+    app = Fastack(
+        debug=settings.DEBUG,
+        routes=routes,
+        title=title,
+        description=description,
+        version=version,
+        openapi_url=openapi_url,
+        openapi_tags=openapi_tags,
+        servers=servers,
+        dependencies=dependencies,
+        default_response_class=default_response_class,
+        docs_url=docs_url,
+        redoc_url=redoc_url,
+        swagger_ui_oauth2_redirect_url=swagger_ui_oauth2_redirect_url,
+        swagger_ui_init_oauth=swagger_ui_init_oauth,
+        middleware=middleware,
+        exception_handlers=exception_handlers,
+        on_startup=on_startup,
+        on_shutdown=on_shutdown,
+        terms_of_service=terms_of_service,
+        contact=contact,
+        license_info=license_info,
+        openapi_prefix=openapi_prefix,
+        root_path=root_path,
+        root_path_in_servers=root_path_in_servers,
+        responses=responses,
+        callbacks=callbacks,
+        deprecated=deprecated,
+        include_in_schema=include_in_schema,
+        **extra,
     )
-    kwds["debug"] = settings.DEBUG
-    app = Fastack(**kwds)
     app.add_middleware(MergeAppStateMiddleware)
     app.set_settings(settings)
     app.load_plugins()
