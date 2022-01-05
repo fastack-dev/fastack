e3dc950c3f964bdebd6ad986e77205071040837b
aprilahijriyan <37798612+aprilahijriyan@users.noreply.github.com>
2022-01-06 03:59:58 +0700

    📖 DOC: update example project

--- examples/helloworld/app/main.py
+++ examples/helloworld/app/main.py
@@ -1,5 +1,6 @@
 from app import models, settings  # noqa
 from app.controllers import init_controllers
+from app.plugins.globalvar import say_hello
 
 from fastack import create_app
 from fastack.globals import websocket
@@ -10,5 +11,10 @@ init_controllers(app)
 
 @app.websocket("/ws")
 async def ws():
+    if say_hello:
+        km = "beautiful girl"
+    else:
+        km = "cuk"
+
     await websocket.accept()
-    await websocket.send_json({"hi": "there!"})
+    await websocket.send_json({"hi": km})
