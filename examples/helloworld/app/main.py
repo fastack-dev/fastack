51e838cab5a6518d29de3bffd1384dfb737752aa
aprilahijriyan <37798612+aprilahijriyan@users.noreply.github.com>
2022-01-06 03:15:19 +0700

    📖 DOC: Add an example of using the global variable "websocket"

--- examples/helloworld/app/main.py
+++ examples/helloworld/app/main.py
@@ -2,6 +2,13 @@ from app import models, settings  # noqa
 from app.controllers import init_controllers
 
 from fastack import create_app
+from fastack.globals import websocket
 
 app = create_app(settings)
 init_controllers(app)
+
+
+@app.websocket("/ws")
+async def ws():
+    await websocket.accept()
+    await websocket.send_json({"hi": "there!"})
