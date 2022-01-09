ff692ed89466cc2a7caa2e319818796dd82eb77b
aprilahijriyan <37798612+aprilahijriyan@users.noreply.github.com>
2022-01-09 13:48:00 +0700

    💥 NEW: Added function to check context

--- fastack/globals.py
+++ fastack/globals.py
@@ -27,3 +27,27 @@ current_app: "Fastack" = LocalProxy(lambda: _app_ctx_stack.top)
 request: Request = LocalProxy(lambda: _request_ctx_stack.top)
 websocket: WebSocket = LocalProxy(lambda: _websocket_ctx_stack.top)
 state: State = LocalProxy(lambda: _get_state())
+
+
+def has_app_context():
+    """
+    Check if application context is active.
+    """
+
+    return bool(current_app)
+
+
+def has_request_context():
+    """
+    Check if request context is active.
+    """
+
+    return bool(request)
+
+
+def has_websocket_context():
+    """
+    Check if websocket context is active.
+    """
+
+    return bool(websocket)
